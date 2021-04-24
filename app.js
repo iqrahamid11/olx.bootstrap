794a9b8b99e70fc98e0b8fd58e6f213a0b240d53
Iqra Hamid <hamidiqra55@gmail.com>
2021-04-24 12:09:13 -0700

    Work Accomplish

---
+++ app.js
@@ -0,0 +1,102 @@
+// alert ("Hello World");
+
+// var a = 5;
+// var b = 6;
+// var c = a+b;
+
+// alert (c);
+
+// var apple = 13;
+// var banana = 12;
+// var fruit = (apple+banana);
+
+// alert (fruit);
+
+// var a = 5;
+// var b = 6;
+// var c = a+b;
+
+// console.log (c);
+
+// var x = 3;
+// var y = "dua";
+// var z = (y+x);
+
+// alert(z);
+
+// var x = 3;
+// var y = "dua";
+// var z = (y+x);
+
+// console.log (z);
+
+// alert ("Thanks for your input!");
+
+// var caseOty = 144;
+// alert (caseOty);
+
+// var popularNumber = 2 + 2;
+// alert (popularNumber);
+
+
+
+// var popularNumber = 12 - 24;
+// alert (popularNumber);
+
+
+
+// var popularNumber = 2 * 4;
+// console.log (popularNumber);
+
+
+// var a = 5;
+// var b = --a + --a - a++ + a-- - a + a;
+// console.log (b);
+
+// 4 + 3 - 3 + 4 - 3 + 3 = 8
+
+// var totalCost = 1 + 3 * 4;
+// alert (totalCost);
+
+var name =("Enter your name");
+var message = "Thanks,";
+var banger = "!";
+alert (name+message+userName+banger);
+// alert("Thanks, " + userName + "!");
+
+
+
+var name = ("Enter your name here");
+alert (name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
